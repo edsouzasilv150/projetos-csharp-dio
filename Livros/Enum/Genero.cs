1cea6ff132fd9c44e2bf42cb06ee0b0b9f6537b6
Edilson Sousa <edsouzasilv@hotmail.com>
2021-06-17 16:28:09 -0300

    Criando a classe Livro e Genero.

---
+++ Livros/Enum/Genero.cs
@@ -0,0 +1,26 @@
+namespace Livros
+{
+    public enum Genero
+    {
+Administração = 1,
+Negócios = 2,
+Economia = 3,
+Arte = 4,
+Cinema = 5,
+Fotografia = 6,
+Autoajuda = 7,
+Biografias = 8,
+Ciências = 9,
+Informática = 10,
+Entretenimento = 11,
+Direito = 12, 
+Educação = 13,
+Infantil = 14,
+Espostes = 15,
+Gastronomia = 16,
+Ficção = 17,
+Medicina = 18,
+Culinária = 19,
+Mangás = 20
+    }
+}
